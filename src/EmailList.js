b66dd60e6a0ae837a9e6b2cc8cb2baf9ca1437b7
Ethan Salonga <ethansalonga28@gmail.com>
2022-10-31 12:47:13 -0400

    Build email list settings

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import "./EmailList.css"
 import { Checkbox, IconButton } from '@mui/material'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import RedoIcon from '@mui/icons-material/Redo';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
+import SettingsIcon from '@mui/icons-material/Settings';
+import "./EmailList.css"
 
 function EmailList() {
   return (
@@ -8,8 +15,33 @@ function EmailList() {
       <div className="emailList__settings">
         <div className="emailList__settingsLeft">
           <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
         </div>
       </div>
+
+      
     </div>
   )
 }
