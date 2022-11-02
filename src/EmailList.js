dd587cf609c58ec32c348f2deba1c387659cd75b
Ethan Salonga <ethansalonga28@gmail.com>
2022-11-02 16:15:10 -0400

    Add OpenMail in redux

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Checkbox, IconButton } from '@mui/material'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import RedoIcon from '@mui/icons-material/Redo'
@@ -13,8 +13,18 @@ import LocalOfferIcon from '@mui/icons-material/LocalOffer'
 import "./EmailList.css"
 import Section from "./Section"
 import EmailRow from './EmailRow'
+import { db } from "./firebase"
 
 function EmailList() {
+  const [emails, setEmails] = useState([])
+
+  useEffect(() => {
+    db.collection("emails").orderBy("timestamp", "desc").onSnapshot(snapshot => setEmails(snapshot.docs.map(doc => ({
+      id: doc.id,
+      data: doc.data()
+    }))))
+  }, [])
+
   return (
     <div className="emailList">
       <div className="emailList__settings">
@@ -53,18 +63,16 @@ function EmailList() {
       </div>
 
       <div className="emailList__list">
-        <EmailRow
-          title="Twitch"
-          subject="Hello streamers"
-          description="This is a test"
-          time="10pm"
-        />
-        <EmailRow
-          title="Twitch"
-          subject="Hello streamers"
-          description="This is a test"
-          time="10pm"
-        />
+        {emails.map(({ id, data: { to, subject, message, timestamp } }) => (
+          <EmailRow
+            id={id}
+            key={id}
+            title={to}
+            subject={subject}
+            description={message}
+            time={new Date(timestamp?.seconds * 1000).toUTCString()}
+          />
+        ))}
       </div>
     </div>
   )
--- src/EmailRow.js
+++ src/EmailRow.js
@@ -4,12 +4,23 @@ import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
 import LabelImportantOutlinedIcon from '@mui/icons-material/LabelImportantOutlined'
 import "./EmailRow.css"
 import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { selectMail } from "./features/mailSlice"
 
 function EmailRow({ id, title, subject, description, time }) {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const openMail = () => {
+    dispatch(selectMail({
+      id, title, subject, description, time
+    }))
+
+    navigate("/mail")
+  }
 
   return (
-    <div onClick={() => navigate("/mail")} className="emailRow">
+    <div onClick={openMail} className="emailRow">
       <div className="emailRow__options">
         <Checkbox />
         <IconButton>
