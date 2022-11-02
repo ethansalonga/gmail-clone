2a1be650f182d1f2393aca4df46aebef4ed604ca
Ethan Salonga <ethansalonga28@gmail.com>
2022-11-02 17:37:55 -0400

    Setup GoogleAuth login and logout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
@@ -7,30 +7,52 @@ import EmailList from './EmailList'
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom"
 import SendMail from './SendMail'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectSendMessageIsOpen } from './features/mailSlice'
+import { login, selectUser } from './features/userSlice'
+import Login from './Login'
+import { auth } from './firebase'
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL
+        }))
+      } else {
+
+      }
+    })
+  }, [])
 
   return (
     <Router>
-      <div className="app">
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Routes>
-            <Route path="/mail" element={<Mail />} />
-            <Route path="/" element={<EmailList />} />
-          </Routes>
-        </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Routes>
+              <Route path="/mail" element={<Mail />} />
+              <Route path="/" element={<EmailList />} />
+            </Routes>
+          </div>
 
-        {sendMessageIsOpen && <SendMail />}
-      </div>
+          {sendMessageIsOpen && <SendMail />}
+        </div>
+      )}
     </Router>
   )
 }
